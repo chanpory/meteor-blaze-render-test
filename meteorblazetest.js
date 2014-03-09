d78bcf37c41ce57dfeeb4aadb544d466eedf9822
Chanpory Rith <hello@chanpory.com>
2014-03-08 19:41:02 -0800

    Init

---
+++ meteorblazetest.js
@@ -0,0 +1,34 @@
+if (Meteor.isClient) {
+  Template.comments.helpers({
+    comments: function() {
+      return Comments.find().fetch();
+    }
+  });
+
+  Template.comments.events({
+    'click input': function () {
+      Meteor.call("insertComment", {body:""});
+    }
+  });
+
+  Template.comment.rendered = function(){
+    //This should only fire once.
+    console.log("rendered");
+  }
+}
+
+
+// Collections
+Comments = new Meteor.Collection('comments');
+
+// Methods
+Meteor.methods({
+  insertComment: function(commentAttributes) {
+    var comment = _.extend(commentAttributes, {
+      body: "Test comment",
+      submitted: new Date().getTime()
+    });
+
+    return Comments.insert(comment);
+  }
+});
